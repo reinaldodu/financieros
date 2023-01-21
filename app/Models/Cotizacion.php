942f9728ba986976f89994b735832ef9656d15bf
reinaldodu <reinaldodu@gmail.com>
2023-01-20 19:37:41 -0500

    Clase Laravel Proyecto Financieros

---
+++ app/Models/Cotizacion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cotizacion extends Model
+{
+    use HasFactory;
+    //Tabla cotizaciones
+    protected $table = 'cotizaciones';
+}
